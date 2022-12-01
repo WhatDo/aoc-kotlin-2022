7604232c8da8aca3900b7dc4f3d5d3efaacebb5c
Jonas <jp@veo.co>
2022-12-01 09:58:52 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,31 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+
+    val (maxCalories, _) = input.fold(0 to 0) { (top, sum), s ->
+        if (s.isBlank()) {
+            maxOf(top, sum) to 0
+        } else {
+            top to sum + s.toInt()
+        }
+    }
+
+    println("Highest sum of calories: $maxCalories")
+
+
+    val (topThree, _) = input.fold(TopThree() to 0) { (top, sum), s ->
+        if (s.isBlank()) {
+            top.with(sum) to 0
+        } else {
+            top to sum + s.toInt()
+        }
+    }
+
+    println("Sum of the top three calories: ${topThree.values.sum()}")
+}
+
+
+class TopThree(
+    val values: List<Int> = mutableListOf(0, 0, 0)
+) {
+    fun with(x0: Int) = TopThree((values + x0).sortedDescending().take(3))
 }
